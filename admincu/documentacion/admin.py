7b6b5dc0ac3719696ced64cb4983d0cfc13d1e35
Tomas <rtomasfr1020@gmail.com>
2026-02-27 13:17:16 -0300

    importador actas django-admin

--- admincu/documentacion/admin.py
+++ admincu/documentacion/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from import_export import fields, resources
+from import_export.admin import ImportExportModelAdmin
+from import_export.widgets import ForeignKeyWidget
 
-# Register your models here.
+from arquitectura.models import Consorcio
+from .models import ActaConsejo
+
+
+class ActaConsejoResource(resources.ModelResource):
+    consorcio = fields.Field(
+        column_name="consorcio",
+        attribute="consorcio",
+        widget=ForeignKeyWidget(Consorcio, "id"),
+    )
+
+    class Meta:
+        model = ActaConsejo
+        fields = (
+            "consorcio",
+            "nombre",
+            "fecha",
+            "numero",
+            "foja",
+            "contenido",
+            "descripcion",
+            "firma",
+            "transcripcion",
+        )
+        import_id_fields = ("consorcio", "nombre", "fecha", "numero", "foja")
+        skip_unchanged = True
+        report_skipped = True
+
+
+@admin.register(ActaConsejo)
+class ActaConsejoAdmin(ImportExportModelAdmin):
+    resource_class = ActaConsejoResource
+    list_display = ("nombre", "consorcio", "fecha", "numero", "foja", "firma", "transcripcion")
+    list_filter = ("consorcio", "firma", "transcripcion")
+    search_fields = ("nombre", "descripcion")
